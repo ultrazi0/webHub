762b4b91c83a54524c127db3bc1de9899dc17e4c
ultrazi0 <127105309+ultrazi0@users.noreply.github.com>
2025-01-26 20:52:33 +0100

    Move integration test to a separate source set

--- src/integrationTest/java/com/nemo/webHub/WebHubApplicationTests.java
+++ src/integrationTest/java/com/nemo/webHub/WebHubApplicationTests.java
@@ -1,8 +1,6 @@
 package com.nemo.webHub;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)

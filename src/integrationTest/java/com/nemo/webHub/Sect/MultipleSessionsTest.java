ed62b09d64d8adb6d8d639395137c81bc415da75
ultrazi0 <127105309+ultrazi0@users.noreply.github.com>
2025-02-01 14:01:55 +0100

    Fix login integration tests

--- src/integrationTest/java/com/nemo/webHub/Sect/MultipleSessionsTest.java
+++ src/integrationTest/java/com/nemo/webHub/Sect/MultipleSessionsTest.java
@@ -7,14 +7,19 @@
 
 package com.nemo.webHub.Sect;
 
+import com.nemo.webHub.Decibel.UserEntity;
+import com.nemo.webHub.Decibel.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
@@ -24,43 +29,71 @@ import static org.springframework.security.test.web.servlet.response.SecurityMoc
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MultipleSessionsTest {
+
+    private static final String loginUri = "/api/login";
+    private static final String userUri = "/api/user";
+
+    private static final String username = "testUser";
+    private static final String password = "testPassword";
+
+    private UserEntity user;
 
     @Autowired
     private MockMvc mvc;
-    private final String loginUri = "/api/login";
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setup() {
+        user = userRepository.addNewUser(username, passwordEncoder.encode(password));
+    }
+
+    @AfterEach
+    public void teardown() {
+        userRepository.deleteUser(user.getId());
+    }
 
     @Test
     void testLogin() throws Exception {
-        mvc.perform(formLogin(loginUri)).andExpect(authenticated());
+        mvc.perform(formLogin(loginUri).user(username).password(password)).andExpect(authenticated());
     }
 
     @Test
     void testLogout() throws Exception {
-        RequestBuilder loginRequest = formLogin(loginUri);
-        MvcResult loginResult = mvc.perform(loginRequest).andExpect(authenticated()).andReturn();
+        MvcResult loginResult = mvc
+            .perform(formLogin(loginUri).user(username).password(password))
+            .andExpect(authenticated())
+            .andReturn();
+
         MockHttpSession session = (MockHttpSession) loginResult.getRequest().getSession();
         Assert.notNull(session, "Mock session cannot be null");
 
         mvc.perform(post("/api/logout").session(session).with(csrf()));
 
-        mvc.perform(get("/api/user").session(session)).andExpect(unauthenticated());
+        mvc.perform(get(userUri).session(session)).andExpect(unauthenticated());
     }
 
     @Test
     void loginOnSecondLoginThenFirstSessionTerminated() throws Exception {
-        MvcResult firstLoginResult = mvc.perform(formLogin(loginUri)).andExpect(authenticated()).andReturn();
+        MvcResult firstLoginResult = mvc
+            .perform(formLogin(loginUri).user(username).password(password))
+            .andExpect(authenticated())
+            .andReturn();
 
         MockHttpSession firstLoginSession = (MockHttpSession) firstLoginResult.getRequest().getSession();
 
         Assert.notNull(firstLoginSession, "Mock session cannot be null");
-        mvc.perform(get("/api/user").session(firstLoginSession)).andExpect(authenticated());
+        mvc.perform(get(userUri).session(firstLoginSession)).andExpect(authenticated());
 
-        mvc.perform(formLogin(loginUri)).andExpect(authenticated());
+        mvc.perform(formLogin(loginUri).user(username).password(password)).andExpect(authenticated());
 
         // First session is terminated by second login
-        mvc.perform(get("/api/user").session(firstLoginSession)).andExpect(unauthenticated());
+        mvc.perform(get(userUri).session(firstLoginSession)).andExpect(unauthenticated());
     }
 }

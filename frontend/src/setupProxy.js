992cebbb96cd608849a9462e93429f458c770b62
ultrazi0 <127105309+ultrazi0@users.noreply.github.com>
2025-02-02 22:52:00 +0100

    Set proxy via an environment variable

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const proxyHost = process.env.REACT_APP_PROXY_HOST;
+
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: "http://localhost:8080/api",
+            target: proxyHost ? proxyHost + "/api" : "http://localhost:8080/api",
             changeOrigin: true,
         })
     )
